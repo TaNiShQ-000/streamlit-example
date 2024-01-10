3a7119f5309eab1786ee7d8228cd8700eef314f1
TaNiShQ-000 <76695054+TaNiShQ-000@users.noreply.github.com>
2024-01-10 11:39:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,14 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-# import torch
-# from transformers import pipeline
 
-# device = "cuda:0" if torch.cuda.is_available() else "cpu"
-# pipe = pipeline(
-#     "automatic-speech-recognition", model="openai/whisper-large-v3", device=device
-# )
+import torch
+from transformers import pipeline
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+pipe = pipeline(
+    "automatic-speech-recognition", model="openai/whisper-large-v3", device=device
+)
 
 st.write("What is data science ? ")
 
@@ -24,12 +25,3 @@ if audio_file is not None:
     
     st.write("Processing Result:", result)
 
-
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
-#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-#     ))
