1cdec6addf66518a754ea063dfc4b7c31c293566
TaNiShQ-000 <76695054+TaNiShQ-000@users.noreply.github.com>
2024-01-10 16:35:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,27 @@
 # import altair as alt
 # import numpy as np
 # import pandas as pd
-# import streamlit as st
 
-# import torch as tc
-# from transformers import pipeline
+import torch as tc
+from transformers import pipeline
 
-# device = "cuda:0" if tc.cuda.is_available() else "cpu"
-# pipe = pipeline(
-#     "automatic-speech-recognition", model="openai/whisper-large-v3", device=device
-# )
+device = "cuda:0" if tc.cuda.is_available() else "cpu"
+pipe = pipeline(
+    "automatic-speech-recognition", model="openai/whisper-large-v3", device=device
+)
 
-# st.write("What is data science ? ")
+st.write("What is data science ? ")
 
-# audio_file = st.file_uploader("student_recording", type=["wav"])
+audio_file = st.file_uploader("student_recording", type=["wav"])
 
 
-# if audio_file is not None:
-#     # Read the content only if a file is uploaded
-#     audio_content = audio_file.read()
+if audio_file is not None:
+    # Read the content only if a file is uploaded
+    audio_content = audio_file.read()
     
-#     # Process audio using your module
-#     result = pipe(audio_content, max_new_tokens=256)
+    # Process audio using your module
+    result = pipe(audio_content, max_new_tokens=256)
     
-#     st.write("Processing Result:", result)
+    st.write("Processing Result:", result)
 
-import streamlit as st
-st.write('# Streamlit calculator')
-number1= st.number_input('number 1')
-number2 = st.number_input('number 2')
-num3 = number1+number2
-st.write('# Answer is ',num3)
+# import streamlit as 
